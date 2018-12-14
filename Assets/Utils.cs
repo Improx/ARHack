596a9683bdcc05580083b21ed23951ea6f0b61c7
Max Samarin <maxim.samarin@gmail.com>
2018-12-14 18:11:46 +0200

    Distance between two GPS coords

---
+++ Assets/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils {
+
+	//TODO: Radius varies in reality. For more exact esults, find Earth's radius in Espoo
+	public static float EarthRadius = 6371000f;
+
+	public static float DistanceGPS(LocationInfo gps1, LocationInfo gps2)
+	{
+		var lat1 = gps1.latitude * Mathf.Deg2Rad;
+		var lat2 = gps2.latitude * Mathf.Deg2Rad;
+		var delta_lat = lat2 - lat1;
+
+		var long_1 = gps1.longitude * Mathf.Deg2Rad;
+		var long_2 = gps2.longitude * Mathf.Deg2Rad;
+		var delta_long = long_2 - long_1;
+
+		var a = Mathf.Sin(delta_lat / 2) * Mathf.Sin(delta_lat / 2) +
+				Mathf.Cos(lat1) * Mathf.Cos(lat2) *
+				Mathf.Sin(delta_long / 2) * Mathf.Sin(delta_long / 2);
+		var c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+
+		var distance = EarthRadius * c;
+		return distance;
+	}
+
+}
