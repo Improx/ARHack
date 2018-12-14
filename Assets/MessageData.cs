e81a9c8ea3183e3ce2da99d6d2e8ae5cc6ecd8fc
Oskiii <oliukku@gmail.com>
2018-12-14 18:40:39 +0200

    Started message service

---
+++ Assets/MessageData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageData : MonoBehaviour
+{
+	public string Id;
+	public LocationInfo Location;
+	public string Message;
+}
---
+++ Assets/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MessageService
+{
+	public const string ServerUrl = "http://www.my-server.com";
+
+	public static async void GetMessagesAroundAsync(LocationInfo coordinates, float radius)
+	{
+		using(UnityWebRequest www = UnityWebRequest.Get(ServerUrl))
+		{
+			Debug.Log("call");
+			await www.SendWebRequest();
+
+			if (www.isNetworkError || www.isHttpError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				// Show results as text
+				Debug.Log(www.downloadHandler.text);
+
+				// TODO: Check this
+				JsonUtility.FromJson<MessageData[]>(www.downloadHandler.text);
+
+				// Or retrieve results as binary data
+				byte[] results = www.downloadHandler.data;
+			}
+		}
+	}
+}
