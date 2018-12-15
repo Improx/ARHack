53a92f57ff13cc2adf0fef068a17bd239db6c28f
Oskiii <oliukku@gmail.com>
2018-12-15 13:34:11 +0200

    message testing script

---
+++ Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	private void Start()
+	{
+		print("start");
+		Input.location.Start();
+		//MessageService.GetMessagesAroundAsync(Input.location.lastData, 5f);
+
+		MessageService.SaveMessage(
+			new MessageData
+			{
+				text = "Moi",
+					location = new Location
+					{
+						coordinates = new float[] { 1, 2 }
+					},
+					altitude = 300,
+					frames = new BoneTransform[][]
+					{
+						new BoneTransform[]
+						{
+							new BoneTransform
+							{
+								rotation = new float[] { 1f, 1f, 1f, 1f },
+									position = new float[] { 1f, 1f, 1f },
+							}
+						}
+					}
+			});
+	}
+}
