f6be6d598b9f7a993ca1470c5db738229d2a2872
Oskiii <oliukku@gmail.com>
2018-12-15 16:38:10 +0200

    Loading message for ui

---
+++ Assets/LoadingMessage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoadingMessage : MonoBehaviour
+{
+	[SerializeField]
+	private TextMeshProUGUI _loadingText;
+
+	public void SetLoadingText(string text = "Uploading...")
+	{
+		_loadingText.text = text;
+	}
+
+	public void ClearLoadingText()
+	{
+		_loadingText.text = string.Empty;
+	}
+}
