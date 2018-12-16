1b049422ee1a0cbdbd2fa651bbec2df392e3036a
Oskiii <oliukku@gmail.com>
2018-12-16 13:38:55 +0200

    Showskeleton button

---
+++ Assets/ShowSkeletonButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BodyARSample;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowSkeletonButton : MonoBehaviour
+{
+
+	[SerializeField]
+	private Image _crossImage;
+	private BodyARController _bodyDrawer;
+
+	private void Start()
+	{
+		_bodyDrawer = FindObjectOfType<BodyARController>();
+	}
+
+	public void ToggleSkeleton()
+	{
+		_bodyDrawer.ToggleDrawBody();
+		_crossImage.enabled = !_crossImage.enabled;
+	}
+}
